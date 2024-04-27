2e9e8534bccf3ac2f254320d3a048e098c0d5952
Ulises <ulyses.sosa@gmail.com>
2024-04-27 02:11:15 +0200

    Fix bug for pick up intention with no path found. Mark it for deletion  when failed

--- src/lib/plans/GoPickUp.js
+++ src/lib/plans/GoPickUp.js
@@ -15,22 +15,26 @@ export default class GoPickUp extends Plan {
     }
 
     async execute(predicate) {
-        // console.log('GoPickUp.execute: predicate ', predicate, ' me ', me);
+        console.log('GoPickUp.execute: predicate ', predicate, ' me ', me);
 
         let path = await this.subIntention('a_star', [predicate.x, predicate.y]);
+     
+        if (path.length === 0) {
+            agent.changeIntentionScore('go_pick_up', [predicate], -1, predicate.id);
+
+            throw ['No path found'];
+        }
+     
         path = path.reverse();
         path.shift();
-
         await this.subIntention('follow_path', [path]);
-        let pickup = await client.pickup();
+        await client.pickup();
 
-        // if (pickup.length > 0) {
         let id = predicate.id
         let reward = parcels.has(id) ? parcels.get(id).reward : predicate.reward
         carryParcel({ id, reward });
         parcels.delete(id);
-        agent.changeIntentionScore('go_pick_up', [predicate], -1, 'go_pick_up');
-        // }
+        agent.changeIntentionScore('go_pick_up', [predicate], -1, predicate.id);
 
         if (this.stopped) throw ['stopped']; // if stopped then quit
 
