d898f00635932bb09574fe9f1ce95a1a7973b889
Lorenzo <lorenzofumi@gmail.com>
2024-05-20 13:16:54 +0200

    Pass the name of the token variable as argument

--- src/lib/utils/client.js
+++ src/lib/utils/client.js
@@ -4,9 +4,18 @@ import { me } from "./utils.js";
 
 dotenv.config();
 
+let token = process.env.TOKEN;
+
+/**
+ * Pass the name of the token variable as argument
+ */
+if (process.argv.length > 2) {
+    token = process.env[process.argv[2]];
+}
+
 const client = new DeliverooApi(
     process.env.URL || `http://localhost:8080`,
-    process.env.TOKEN,
+    token,
 );
 export const askPartnerId = (partnerName) => client.shout(partnerName).then(console.log);
 export const passOwnId = (partnerId) => client.say(partnerId, me.id);
